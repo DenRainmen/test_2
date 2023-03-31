a511d63f0ca3c4da4ddc2859c9deb852a3b7c855
Den <potarykindenis@gmail.com>
2023-04-01 00:09:34 +0300

    add russian text

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Нажмите на Vite и React логотипы, чтобы узнать больше.
       </p>
     </div>
   )
